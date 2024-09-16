f8800aeaa9ef9c7db1d03225564635149d7653d6
derek0k <derek0kim@gmail.com>
2024-09-16 23:53:08 +0900

    Header에 Logout 버튼 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import { Link, useNavigate } from "react-router-dom";
 
-export default function Header({ handleLogout, user }) {
+export default function Header({ user, setUser }) {
   const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    setUser(null);
+    navigate("/");
+  };
 
   return (
     <header>
