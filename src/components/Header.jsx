a6d091ef2a10ec3480989d334f48b2aa03d3c0b2
derek0k <derek0kim@gmail.com>
2024-09-18 00:36:53 +0900

    fix: /results로 이동하는 링크 만들기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,6 +16,7 @@ export default function Header({ user, setUser }) {
         <div>
           {user ? (
             <>
+              <Link to="/results">테스트 히스토리</Link>
               <Link to="/test">테스트</Link>
               <Link to="/profile">프로필</Link>
               <div>{user.nickname} 님</div>
