ad6f8d9ed6e6b6af25bb16322955c76ac8236000
derek0k <derek0kim@gmail.com>
2024-09-17 17:46:27 +0900

    fix: 로그인 후 홈 화면에서 로그인 버튼 표시되지 않도록 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -13,7 +13,7 @@ export default function Router({ user, setUser }) {
     <BrowserRouter>
       <Layout user={user} setUser={setUser}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home user={user} />} />
           <Route path="/login" element={<Login setUser={setUser} />} />
           <Route path="/signup" element={<Signup />} />
           <Route
