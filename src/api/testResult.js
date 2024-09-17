18848ed2a49f3adf88e6e92f40e0f3a99ba745b0
derek0k <derek0kim@gmail.com>
2024-09-17 19:42:07 +0900

    fix: 환경 변수를 읽어올 수 있도록 VITE를 접두사 붙이기

--- src/api/testResult.js
+++ src/api/testResult.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.JSON_SERVER_API_URL + "/testResults";
+const API_URL = import.meta.env.VITE_JSON_SERVER_API_URL + "/testResults";
 
 export const getTestResults = async () => {
   const response = await axios.get(API_URL);
