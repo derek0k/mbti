ec2e027423fbcf59748884715dd5c7b16fe1c4f9
derek0k <derek0kim@gmail.com>
2024-09-18 11:29:29 +0900

    ProfilePage 예외처리 추가

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -5,7 +5,7 @@ import { useUserStore } from "../store/userStore";
 export default function ProfilePage() {
   const { user, setUser } = useUserStore();
 
-  const [nickname, setNickname] = useState(user?.nickname || "");
+  const [nickname, setNickname] = useState("");
 
   const handleNicknameChange = (e) => {
     setNickname(e.target.value);
@@ -13,10 +13,17 @@ export default function ProfilePage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!nickname.trim()) {
+      alert("변경할 닉네임을 입력해 주세요");
+      return;
+    }
+
     try {
       const response = await updateProfile({ nickname });
       setUser({ ...user, nickname: response.nickname });
       alert("프로필이 성공적으로 변경되었습니다");
+      setNickname("");
     } catch (e) {
       alert("닉네임 변경에 실패했습니다");
     }
@@ -24,16 +31,18 @@ export default function ProfilePage() {
 
   return (
     <div>
-      <div>
-        <h1>프로필 수정</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>닉네임</label>
-            <input onChange={handleNicknameChange} />
-          </div>
-          <button type="submit">프로필 업데이트</button>
-        </form>
-      </div>
+      <h1>프로필 수정</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>닉네임</label>
+          <input
+            onChange={handleNicknameChange}
+            value={nickname}
+            placeholder={user.nickname || ""}
+          />
+        </div>
+        <button type="submit">프로필 업데이트</button>
+      </form>
     </div>
   );
 }
